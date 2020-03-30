47bff3e1a0e0a31299bacb72513af73c40dcf5b3
Sanmoh Hombal <devsanmoh@gmail.com>
2020-03-30 22:53:00 +0530

    Refactor, Animation Removed, Scroll to top

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,27 @@
-import React, {Component} from "react";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import {CSSTransition, TransitionGroup} from "react-transition-group";
+import React from "react";
+import {Route, Switch} from "react-router-dom";
 
 import Home from "./Home";
 import About from "./About";
 import Work from "./Work";
 import Stack from "./Stack";
 import Contact from "./Contact";
+import ScrollToTop from "./ScrollToTop";
 
 import "../styles/app.scss";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/work" component={Work} />
-            <Route path="/stack" component={Stack} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <ScrollToTop>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/work" component={Work} />
+        <Route path="/stack" component={Stack} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </ScrollToTop>
+  );
 }
 
 export default App;
